036c04d613f505dca97e8bcfaf24bdb3a2ad7b47
nafis195 <nafisbuet1@gmail.com>
2020-04-14 23:01:13 -0700

    added js

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ Time - 7:14 PM
 
 
 
+function showNumber(number){
+    let existingNumber = $("#result").val();
+    $("#result").val(existingNumber + number);
+}
+
+
+function clearNumber(){
+    $("#result").val('');
+}
+
+
+function calculate(){
+    let result = eval($("#result").val());
+    $("#result").val(result);
+}
+
+
+function deleteNumber(){
+    let monitorNumber = $("#result").val();
+    $("#result").val(monitorNumber.slice(0, -1));
+}
